f9bf93ae11330a7df538181e1c4eb2e9ae16fd0e
Sk031397 <sk031397.scholar@gmail.com>
2022-04-21 03:25:08 +0800

    added_openai

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,7 @@ import styles from "./index.module.css";
 export default function Home() {
   const [result, setResult] = useState();
   const [codeInput,setCodeInput] = useState();
+  const [textInput,setTextInput] = useState();
   async function onSubmit(event) {
     event.preventDefault();
     const response = await fetch("/api/generate", {
@@ -16,7 +17,8 @@ export default function Home() {
     });
     const data = await response.json();
     setResult(data.result);
-	   setCodeInput("");
+	  setCodeInput("");
+    setTextInput("");
   }
 
   return (
@@ -30,11 +32,18 @@ export default function Home() {
 	   <input
             type="text"
             name="phrase"
-            placeholder="Enter a phrase"
+            placeholder="Enter a piece of text you wnat to make into code"
             value={codeInput}
             onChange={(e) => setCodeInput(e.target.value)}
           />
-          <input type="submit" value="Generate code" />
+          <input
+          type="text"
+          name="phrase_one"
+          placeholder="Enter a piece of text"
+          value={textInput}
+          onChange={(e) => setTextInput(e.target.value)}
+        />
+          <input type="submit" value="Generate" />
         </form>
         <div className={styles.result}>{result}</div>
       </main>
